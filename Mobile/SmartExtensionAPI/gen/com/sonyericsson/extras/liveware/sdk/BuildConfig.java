1e77a641c3b265e2ff5fb6355fc93012a2b1e537
NikolaSmrcek <zipidus@gmail.com>
2015-01-09 16:25:33 +0100

    Revert "Mobilna aplikacija"
    This reverts commit abb9827f7e83f776ef096312159bf1e78de54b2e.

--- Mobile/SmartExtensionAPI/gen/com/sonyericsson/extras/liveware/sdk/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.sonyericsson.extras.liveware.sdk;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
